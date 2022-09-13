f41ed0fb1d9926d47ac13c979ac223e34e40810d
Shazzaam <fifaprofile21@outlook.com>
2022-09-13 18:09:52 +0200

    Update 1
    - Added grabbing of patch list in JSON format provided by ReVanced Team
    - Added ComboBox filled with features that can be excluded from building ReVanced Application
    - Added TextBlock that shows description of a feature that was selected in the ComboBox
    - Added a button that excludes the feature that is selected in ComboBox from building Revanced Application
    - Added ListBox where Excluded features are located
    - Added Include Features button that removes the feature from Excluded Features ListBox

--- Revanced Builder/MainWindow.xaml.cs
+++ Revanced Builder/MainWindow.xaml.cs
@@ -13,6 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using HtmlAgilityPack;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+
 namespace Revanced_Builder
 {
     /// <summary>
@@ -23,6 +31,125 @@ namespace Revanced_Builder
         public MainWindow()
         {
             InitializeComponent();
+            GrabPatchesList();
+        }
+
+
+        List<Patch> patchesList = new List<Patch>();
+        Dictionary<string, string> appDescription = new Dictionary<string, string>();
+        List<string> ExcludedFeatures = new List<string>();
+        //Computer\HKEY_LOCAL_MACHINE\SOFTWARE\Azul Systems\Zulu\zulu-17 - Location of registry for ZULU
+
+        //Events
+        private void Youtube_Features_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (YoutubeFeatures.SelectedIndex < 0)
+            {
+                YoutubeFeatures.SelectedIndex = 0;
+            }
+            if (YoutubeFeatures.Items.Count < 1)
+            {
+                YoutubeFeatureDescription.Text = "";
+                return;
+            }
+            string tempName;
+            YoutubeFeatureDescription.Text = "";
+            try
+            {
+                tempName = YoutubeFeatures.SelectedItem.ToString();
+            }
+            catch 
+            {
+                return;
+            }
+            foreach (string key in appDescription.Keys)
+            {
+                if (key == tempName)
+                {
+                    YoutubeFeatureDescription.Text = "Feature Description\n" + appDescription[key];
+                }
+            }
+        }
+
+        private void YoutubeExcludedFeaturesList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (YoutubeExcludedFeaturesList.Items.Count < 0)
+            {
+                return;
+            }
+        }
+
+        private void YoutubeExcludeFeatureButton_Click(object sender, RoutedEventArgs e)
+        {
+            string SelectedFeature;
+            if (YoutubeFeatures.Items.Count < 1)
+            {
+                return;
+            }
+            try
+            {
+                SelectedFeature = YoutubeFeatures.SelectedItem.ToString();
+            }
+            catch 
+            {
+                return;
+            }
+            YoutubeExcludedFeaturesList.Items.Add(SelectedFeature);
+            YoutubeFeatures.Items.Remove(SelectedFeature);
+        }
+
+        private void YoutubeIncludeFeatureButton_Click(object sender, RoutedEventArgs e)
+        {
+            string SelectedFeature;
+            if (YoutubeExcludedFeaturesList.Items.Count < 1 || YoutubeExcludedFeaturesList.SelectedIndex < 0)
+            {
+                return;
+            }
+            try
+            {
+                SelectedFeature = YoutubeExcludedFeaturesList.SelectedItem.ToString();
+            }
+            catch
+            {
+                return;
+            }
+            YoutubeFeatures.Items.Add(SelectedFeature);
+            YoutubeExcludedFeaturesList.Items.Remove(SelectedFeature);
+        }
+
+
+        //Methods
+        private async void GrabPatchesList()
+        {
+            HttpClient client = new HttpClient();
+            Uri patchSource = new Uri("https://raw.githubusercontent.com/revanced/revanced-patches/main/patches.json");
+            string JSONAsync = await client.GetStringAsync(patchSource);
+            patchesList = JsonConvert.DeserializeObject<List<Patch>>(JSONAsync);
+            foreach (Patch patch in patchesList)
+            {
+                List<CompatiblePackage> temp = patch.compatiblePackages;
+                appDescription.Add(patch.name, patch.description);
+                foreach (CompatiblePackage item in temp)
+                {
+                    switch (item.name)
+                    {
+                        default:
+                            break;
+                        case "com.google.android.youtube":
+                            YoutubeFeatures.Items.Add(patch.name);
+                            break;
+                        case "com.google.android.apps.youtube.music":
+
+                            break;
+                        case "com.twitter.android":
+
+                            break;
+                        case "com.reddit.frontpage":
+
+                            break;
+                    }
+                }
+            }
         }
     }
 }
---
+++ Revanced Builder/Patch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revanced_Builder
+{
+    public class CompatiblePackage
+    {
+        public string name { get; set; }
+        public List<string> versions { get; set; }
+    }
+
+    public class Option
+    {
+        public string key { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public bool required { get; set; }
+        public List<string> choices { get; set; }
+    }
+    public class Patch
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public string version { get; set; }
+        public bool excluded { get; set; }
+        public bool deprecated { get; set; }
+        public List<Option> options { get; set; }
+        public List<string> dependencies { get; set; }
+        public List<CompatiblePackage> compatiblePackages { get; set; }
+
+    }
+}
